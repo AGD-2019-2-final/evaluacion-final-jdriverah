59175d139e42487c079976fab45faad8e2e09e00
jdriverah <55606623+jdriverah@users.noreply.github.com>
2020-03-04 12:31:43 -0500

    Add files via upload

--- 01-hadoop-50/q07-10/mapper.py
+++ 01-hadoop-50/q07-10/mapper.py
@@ -1,4 +1,7 @@
 import sys
+# >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == "__main__":
+    
+    for line in sys.stdin:
+        sys.stdout.write(line.split("   ")[0].replace("\n","").replace("\t","")+","+line.split("   ")[1]+","+line.split("   ")[2].replace("\t","").replace("\n","")+"\n")
--- 01-hadoop-50/q07-10/reducer.py
+++ 01-hadoop-50/q07-10/reducer.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python
 import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    Clave_act = None
+    misma_letra = []
+
+    for line in sys.stdin:
+        
+        Clave,fecha,valor = line.split(',')
+        
+        if Clave == Clave_act:
+            misma_letra.append([int(valor.replace('\n','')),Clave,fecha])
+
+        else:
+            if Clave_act is not None:
+                misma_letra.sort()
+                for i in misma_letra:
+                    sys.stdout.write(i[1]+'   '+i[2]+'   '+str(i[0])+'\n')
+                misma_letra = []
+            
+            Clave_act = Clave
+            misma_letra.append([int(valor.replace('\n','')),Clave,fecha])
+    misma_letra.sort()
+    for i in misma_letra:
+        sys.stdout.write(i[1]+'   '+i[2]+'   '+str(i[0])+'\n')
