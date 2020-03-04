0cada36d114c02565834883696c3e6f023a937d9
jdriverah <55606623+jdriverah@users.noreply.github.com>
2020-03-04 12:36:03 -0500

    Add files via upload

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -1,5 +1,11 @@
+#! /usr/bin/env python
 import sys
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.replace('\n','')
+        valor, clave=line.split("\t")
+        clave2=clave.split(",")
+        for i in clave2:
+            sys.stdout.write(i+"\t"+str(valor)+"\n")
+    
         
