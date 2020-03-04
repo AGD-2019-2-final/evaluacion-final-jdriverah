617d334a68dad0d83c4a21e3192065b45ad442b9
jdriverah <55606623+jdriverah@users.noreply.github.com>
2020-03-04 12:33:02 -0500

    Add files via upload

--- 01-hadoop-50/q08-10/mapper.py
+++ 01-hadoop-50/q08-10/mapper.py
@@ -1,5 +1,6 @@
+#! /usr/bin/env python
 import sys
-#
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
-#
-
+if __name__ == '__main__':
+    for line in sys.stdin:
+        line = line.replace('\n','')
+        sys.stdout.write(line.split('   ')[0].replace('\t','').replace('\n','')+'\t'+line.split('   ')[2].replace('\t','')+'\n')
--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python
 import sys
 #
-#  >>> Escriba el codigo del mapper a partir de este punto <<<
+#  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    Clave_act = None
+    listasum=[]
+
+    for line in sys.stdin:
+
+        clave, valor = line.split("\t")
+        valor = float(valor)
+
+        if clave == Clave_act:
+            listasum.append(float(valor))
+        else:
+            if Clave_act is not None:
+                suma=sum(listasum)
+                elementos=len(listasum)
+                
+                sys.stdout.write(Clave_act.replace("\n","")+"       "+str(suma)+" "+str(suma/elementos)+"\n")
+            Clave_act = clave    
+            listasum=[]
+            listasum.append(float(valor))
+    suma=sum(listasum)
+    elementos=len(listasum)
+    sys.stdout.write(Clave_act.replace("\n","")+"       "+str(suma)+" "+str(round(suma/elementos,3))+"...\n")
