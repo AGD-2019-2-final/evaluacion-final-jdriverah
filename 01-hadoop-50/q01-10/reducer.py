7021090f3c87fa312df46efc9c347ed6c015b2e6
jdriverah <55606623+jdriverah@users.noreply.github.com>
2020-03-04 12:20:33 -0500

    Add files via upload

--- 01-hadoop-50/q01-10/reducer.py
+++ 01-hadoop-50/q01-10/reducer.py
@@ -2,3 +2,26 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    Clave_act = None
+    total = 0
+
+    for line in sys.stdin:
+
+        clave, valor = line.split("\t")
+        valor = int(valor)
+
+        if clave == Clave_act:
+        
+            total += valor
+        else:
+         
+            if Clave_act is not None:
+           
+                sys.stdout.write("{}\t{}\n".format(Clave_act, total))
+
+            Clave_act = clave
+            total = valor
+
+    sys.stdout.write("{}\t{}\n".format(Clave_act, total))
