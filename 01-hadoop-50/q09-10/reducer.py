3719dca4636b9aa9ddce84b7fc943998278206aa
jdriverah <55606623+jdriverah@users.noreply.github.com>
2020-03-04 12:35:10 -0500

    Add files via upload

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python
 import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    Clave_act = None
+    misma_letra = []
+
+    for line in sys.stdin:
+        
+        Clave,fecha,valor = line.split(',')
+        
+        misma_letra.append([int(valor.replace('\n','')),Clave,fecha])
+
+    misma_letra.sort()
+
+    for i in range(6):
+        sys.stdout.write(misma_letra[i][1]+'   '+misma_letra[i][2]+'   '+str(misma_letra[i][0])+'\n')
